f63c74e68c70cec4084e49690a80ca927e39bafd
jingwen <jinwen.lqh@dtyunxi.com>
2021-07-05 16:45:15 +0800

    feat(plugin.rs): add remote js/css to project static folder

--- src/plugins.rs
+++ src/plugins.rs
@@ -9,22 +9,30 @@ pub fn new<'r>(plugins: Vec<Box<dyn plugin::Plugin<'r>>>) -> plugin::PluginManag
     "/static/index.html" => "../static/index.html",
     "/static/custom.js" => "../static/custom.js",
     "/static/custom.css" => "../static/custom.css",
-    "/static/favicon.ico" => "../static/favicon.ico"
+    "/static/favicon.ico" => "../static/favicon.ico",
+    "/static/bootstrap.min.css" => "../static/bootstrap.min.css",
+    "/static/all.min.css" => "../static/all.min.css",
+    "/static/jquery.min.js" => "../static/jquery.min.js",
+    "/static/crypto-js.min.js" => "../static/crypto-js.min.js",
+    "/static/popper.min.js" => "../static/popper.min.js",
+    "/static/bootstrap.min.js" => "../static/bootstrap.min.js",
+    "/static/clipboard.min.js" => "../static/clipboard.min.js",
+    "/static/bootstrap-notify.min.js" => "../static/bootstrap-notify.min.js"
     );
 
     let base_css_imports = vec![
-        "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css",
-        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css",
+        "/static/bootstrap.min.css",
+        "/static/all.min.css",
         "/static/custom.css",
     ];
 
     let base_js_imports = vec![
-        "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js",
-        "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js",
-        "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js",
-        "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js",
-        "https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.4/clipboard.min.js",
-        "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-notify/0.2.0/js/bootstrap-notify.min.js",
+        "/static/jquery.min.js",
+        "/static/crypto-js.min.js",
+        "/static/popper.min.js",
+        "/static/bootstrap.min.js",
+        "/static/clipboard.min.js",
+        "/static/bootstrap-notify.min.js",
         "/static/custom.js",
     ];
 
